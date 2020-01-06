23707a3f97e38a9029c0c66f956576e52654aba0
sriramtorvi <sriram.torvi@gmail.com>
2020-01-06 19:36:25 +0530

    5 files modified

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html'
 })
 export class ServerElementComponent {
-
+  @Input() element: {type: string, name: string, content: string};
 }
