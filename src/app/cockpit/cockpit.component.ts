00d73437e9ba905b19a9595890aebeb4ca47bd3c
sriramtorvi <sriram.torvi@gmail.com>
2020-01-06 14:48:51 +0530

    2 files modified, 2 added

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html'
+})
+export class CockpitElement {
+
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html'
+})
+export class ServerElementComponent {
+
+}
