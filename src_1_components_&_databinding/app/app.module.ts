320def0043f3bff9cb4fb53dfd7a06447d44158e
sriramtorvi <sriram.torvi@gmail.com>
2020-01-19 22:52:07 +0530

    Added source for Directives deep dive

--- src_1_components_&_databinding/app/app.module.ts
+++ src_1_components_&_databinding/app/app.module.ts
@@ -2,19 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective,
-    BetterHighlightDirective
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
