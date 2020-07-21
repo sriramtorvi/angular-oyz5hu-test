7103947a04f53b6307a5f2ecf47613483c6b8f6d
sriramtorvi <sriram.torvi@gmail.com>
2020-07-21 20:56:06 +0530

    Added missing folders

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { AccountService } from '../accounts.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  
+  constructor(private accountService: AccountService) {}
+
+  onSetTo(status: string) {
+    this.accountService.updateAccount(this.id, status);
+    //this.loggingService.logStatusChange(status);
+  }
+}
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AccountService } from '../accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+})
+export class NewAccountComponent {
+  
+  constructor(private accountService: AccountService) {}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus);
+    //this.loggingService.logStatusChange(accountStatus);
+  }
+}
